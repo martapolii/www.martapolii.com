ef157d2226a375e64b4a38f194ae78d5e7154a7d
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-01 21:46:06 -0400

    Create components for 'My Projects' and 'My Services' pages

---
+++ Portfolio/src/service.jsx
@@ -0,0 +1,14 @@
+/*
+File name: service.jsx
+Student name: Marta Polishchuk
+Student ID: 301432299
+Date: October 1, 2024
+*/
+
+export default function Service() {
+  return (
+    <>
+      <p>My Services</p>
+    </>
+  );
+}
