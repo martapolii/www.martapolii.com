06f7c0dbb199cee6f34dd2c4b6844e41769e1a3f
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-02 03:13:29 -0400

    Add content to About me page

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -8,7 +8,10 @@ Date: October 1, 2024
 export default function About() {
   return (
     <>
-      <p>About</p>
+      <h2>About Me</h2>
+      <p>Name: Marta Polishchuk <br/> Age: 26 <br/></p>
+      <img src="./src/assets/headshot.jpg"></img>
+      <p>Hi, my name is Marta Polishchuk, and I'm currently pursuing a Software Engineering Technology Advanced Diploma at Centennial College. I have hands-on experience in full-stack development, including proficiency in C#, JavaScript, SQL, and web technologies like HTML and CSS. I've developed and maintained websites, designed databases, and collaborated on team projects using Agile and Scrum methodologies. I'm particularly interested in leveraging my technical and problem-solving skills to contribute to a dynamic development team, and I'm seeking a co-op opportunity to further enhance my practical experience.</p>
     </>
   );
 }
