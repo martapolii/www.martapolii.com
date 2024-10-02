0264529a02b2d12f5ab4658323e58905e6e5a797
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-01 21:07:54 -0400

    Add components (.jsx files) for Home, About Contact

---
+++ Portfolio/src/about.jsx
@@ -0,0 +1,14 @@
+/*
+File name: 
+Student name: Marta Polishchuk
+Student ID: 301432299
+Date: October 1, 2024
+*/
+
+export default function About(){
+  return(
+    <>
+      <p>About</p>
+    </>
+  );
+}
---
+++ Portfolio/src/contact.jsx
@@ -0,0 +1,14 @@
+/*
+File name: 
+Student name: Marta Polishchuk
+Student ID: 301432299
+Date: October 1, 2024
+*/
+
+export default function Contact() {
+  return(
+    <>
+      <p>Contact</p>
+    </>
+  );
+}
