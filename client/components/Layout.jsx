83d5817182c49a9d545a3f4d1d9ddd80b303b74c
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-01 23:29:18 -0400

    Misc changes

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -5,21 +5,21 @@ Student ID: 301432299
 Date: October 1, 2024 
  */
 
+// this file manages layout and navigation for the site
+
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-// this file manages layout and navigation for the site
 export default function Layout() {
-  return (
-    <>
-      <h1>My Portfolio</h1>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link> | 
-        <Link to="/education">Education</Link> | 
-        <Link to="/project">Project</Link> | 
-        <Link to="/contact">Contact</Link>
-      </nav>
-      <hr />
-    </>
-  );
+  return (
+    <>
+      <h1>My Portfolio</h1>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/education">Education</Link>| <Link to="/project">Project</Link>| <Link to="/contact">Contact</Link>
+      </nav>
+         <br/>
+      <hr />
+      
+    </>
+    
+  );
 }
