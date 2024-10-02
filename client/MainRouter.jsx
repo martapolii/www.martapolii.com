a8e15f1519ad37f7083761ce83240a2da165cecc
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-01 23:13:37 -0400

    Fix paths

--- client/MainRouter.jsx
+++ client/MainRouter.jsx
@@ -10,25 +10,27 @@ Date: October 1, 2024
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-//importing components 
+// Importing components
 import Home from './components/Home';
-import About from './about';
-import Contact from './contact';
-import Service from './service';
-import Project from './project';
+import About from './src/about';
+import Contact from './src/contact';
+import Service from './src/contact';
+import Project from './src/project';
 import Layout from './components/Layout';
+
 
 const MainRouter = () => {
   return (
     <div>
-      <Routes>
-        {/* routes that render each component*/}
-        <Route exact path='/' element={<Home />} /> 
-        <Route exact path='/about' element={<About />} />
-        <Route exact path='/contact' element={<Contact />} />
-        <Route exact path='/service' element={<Service />} />
-        <Route exact path="/project" element={<Project />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/service' element={<Service />} />
+          <Route exact path='/project' element={<Project />} />
+        </Routes>
+      </Layout>
     </div>
   );
 };
