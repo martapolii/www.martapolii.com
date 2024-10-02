b3c2f3901f8d0ffc2b4d63ce644f24ecf279e344
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-01 23:13:21 -0400

    Update App.js to set up Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,15 +7,20 @@ Date: October 1, 2024
 
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+//import { ThemeProvider } from '@material-ui/styles';
 import MainRouter from '../MainRouter';
-
+//import Contact from './contact'
+//import theme from '../theme';
+//import { hot } from 'react-hot-loader'
 const App = () => {
-  return (
-    <Router>
-      <MainRouter />
-    </Router>
-  );
-};
+ return (
+ <Router>
+ 
+ <MainRouter />
 
+ </Router>
+ );
+};
 export default App;
+
 
