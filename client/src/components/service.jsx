7964cc4f7b154bfe6f0a8f59bbff8261247055d8
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-02 04:25:33 -0400

    Added content to "Service" page

--- client/src/components/service.jsx
+++ client/src/components/service.jsx
@@ -9,6 +9,45 @@ export default function Service() {
   return (
     <>
       <p>Services Offered</p>
+      <ul>
+        <li>
+          <h3>C# Application Development</h3>
+          <img src="./src/assets/c-sharp.png" alt="csharp icon"></img>
+          {/*<a href="https://www.flaticon.com/free-icons/database-management" title="database management icons">Database management icons created by Design Circle - Flaticon</a> */}
+          <p>Proficient in building both console and GUI applications using C# and Visual Studio. Whether you need a simple console application or a more complex user interface, I can help bring your software ideas to life.</p>
+        </li>
+        <li>
+          <h3>JavaScript & Web Development</h3>
+          <img src="./src/assets/java-script.png" alt="javascript icon"></img>
+          {/*<a href="https://www.flaticon.com/free-icons/javascript" title="javascript icons">Javascript icons created by UIUX Mall - Flaticon</a> */}
+          <p>Skilled in JavaScript and front-end development, applying DOM manipulation, event handling, and responsive design to create dynamic, interactive websites. I specialize in crafting responsive, user-friendly web experiences.</p>
+        </li>
+        <li>
+          <h3>Database Management & Integration</h3>
+          <img src="./src/assets/database-management.png" alt="database icon"></img>
+          {/*<a href="https://www.flaticon.com/free-icons/database-management" title="database management icons">Database management icons created by Design Circle - Flaticon</a>*/}
+          <p>Proficient in SQL query design, database creation, and integrating databases into software applications. I ensure efficient data storage, retrieval, and manipulation for both small and large-scale projects.</p>
+        </li>
+        <li>
+          <h3>Analytical Problem Solving</h3>
+          <img src="./src/assets/brain.png" alt="brain icon"></img>
+          {/*<a href="https://www.flaticon.com/free-icons/brain" title="brain icons">Brain icons created by Freepik - Flaticon</a>*/}
+          <p>Strong problem-solving skills in Discrete Mathematics, including logic, set theory, and Boolean algebra. I apply these analytical abilities to optimize software development processes and solve complex problems.</p>
+        </li>
+        <li>
+          <h3>Unix/Linux Environments</h3>
+          <img src="./src/assets/linux.png" alt="linux icon"></img>
+          {/*<a href="https://www.flaticon.com/free-icons/linux" title="linux icons">Linux icons created by Freepik - Flaticon</a> */}
+          <p>Experienced in scripting, file management, and basic system administration in Unix/Linux environments. I can help automate tasks, manage files, and maintain system stability for server or desktop environments.</p>
+        </li>
+        <li>
+          <h3>Version Control & Collaboration</h3>
+          <img src="./src/assets/social.png" alt="git icon"></img>
+          {/*<a href="https://www.flaticon.com/free-icons/git" title="git icons">Git icons created by pocike - Flaticon</a> */}
+          <p>Proficient in Git for version control and collaborative software development. I ensure clean, organized codebases and seamless collaboration for development teams.</p>
+        </li>
+      </ul>
+
     </>
   );
 }
