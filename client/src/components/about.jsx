37a4fc1315bcdfa50b647375760a031cbc587655
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-02 03:34:01 -0400

    Add content to "About Me" page (headshot, bio, link to resume)

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -10,8 +10,14 @@ export default function About() {
     <>
       <h2>About Me</h2>
       <p>Name: Marta Polishchuk <br/> Age: 26 <br/></p>
-      <img src="./src/assets/headshot.jpg"></img>
-      <p>Hi, my name is Marta Polishchuk, and I'm currently pursuing a Software Engineering Technology Advanced Diploma at Centennial College. I have hands-on experience in full-stack development, including proficiency in C#, JavaScript, SQL, and web technologies like HTML and CSS. I've developed and maintained websites, designed databases, and collaborated on team projects using Agile and Scrum methodologies. I'm particularly interested in leveraging my technical and problem-solving skills to contribute to a dynamic development team, and I'm seeking a co-op opportunity to further enhance my practical experience.</p>
+      <img src="./src/assets/headshot.jpg" alt="Headshot of Marta Polishchuk"></img>
+      <p>
+        Hi, my name is Marta Polishchuk, and I'm currently pursuing a Software Engineering Technology Advanced Diploma at Centennial College. I have hands-on experience in full-stack development, including proficiency in C#, JavaScript, SQL, and web technologies like HTML and CSS. I've developed and maintained websites, designed databases, and collaborated on team projects using Agile and Scrum methodologies. I'm particularly interested in leveraging my technical and problem-solving skills to contribute to a dynamic development team, and I'm seeking a co-op opportunity to further enhance my practical experience.
+        </p>
+      {/* button to view resume */}
+      <a href="MartaPolishchuk_Resume.pdf" target="blank" rel="noopener noreferrer">
+      <button>View Resume</button></a>
     </>
+    
   );
 }
