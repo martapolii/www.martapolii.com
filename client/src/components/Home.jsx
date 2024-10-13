c49a06f7a218ba15bb43f93ce3c0d83ed4002547
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-13 16:18:09 -0400

    Attempt to fix home page buttons that aren't working on remote site

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -17,10 +17,10 @@ export default function Home() {
         I am a second-year <span style={{color: '#FF69B4', fontWeight:'bold'}}>Software Engineering Technology Co-op student</span>. Currently working on full-stack web development, Java programming, and advanced database concepts. Click the links below to learn more about me and my projects.
         </p>
         <div className="buttons">
-          <button><a href="/about">About</a></button>
-          <button><a href="/service">Services</a></button>
-          <button><a href="/project">Projects</a></button>
-          <button><a href="/contact">Contact</a></button>
+          <button><a href="./about">About</a></button>
+          <button><a href="./service">Services</a></button>
+          <button><a href="./project">Projects</a></button>
+          <button><a href="./contact">Contact</a></button>
         </div>
       </section> 
       <img className="CheekyCode" src="/assets/coderFunction.png" alt="C# code snipet of an object of the Coder class defining variables that align with my hard/soft skills"/>
