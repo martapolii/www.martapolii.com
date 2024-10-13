b6e406db4bdea07702e37b3dcd7c60531c6c2eb2
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-13 13:55:08 -0400

    Create middleware to serve static assets

---
+++ server/assets-router.js
@@ -0,0 +1,26 @@
+/* need to create a middleware to manage/serve static assets
+- created a server folder in the root directory
+- created an assets-router.js file in the server folder
+
+*/
+
+// import express and create a router:
+const express = require("express");
+const router = express.Router();
+
+// define the regex for images and videos:
+const imageRegex = /\/.+\.(svg|png|jpg|png|jpeg)$/; // image formats
+const videoRegex = /\/.+\.(mp4|ogv)$/ //video formats
+
+// iterate over the regex patterns and create a route for each:
+router.get(imageRegex, (req, res) => {
+  const filePath = req.path;
+  res.redirect(303, `http://localhost:3000/src${filePath}`);
+});
+router.get(videoRegex, (req, res) => {
+  const filePath = req.path;
+  res.redirect(303, `http://localhost:3000/src${filePath}`);
+});
+
+//export the router:
+module.exports = router; //reference in server.js file
