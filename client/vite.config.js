a11cb32c2a0768f4afe5511535df52556c3c6a9b
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-13 18:46:30 -0400

    Ran npm run build and npm run dev many times while troubleshooting - found that server.js file referenced index.html in client when it should have been in server-side

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: '../client', //setting the root directory
   plugins: [react()],
-  root: './client', //setting the root directory
   build: {
    outDir: '../dist', //setting the output directory
     manifest: true,
     rollupOptions: {
-      input: "./src/main.jsx", //setting the entry point
+      input: "./index.html", //setting the entry point
     },
   },
 });
