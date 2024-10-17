128386a3c992eff2244b666ace93bc24434c8774
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-16 21:44:48 -0400

    Shorten file name to fix path name too long error

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -17,7 +17,7 @@ export default function About() {
             Hi, my name is <span className="bold">Marta Polishchuk</span>, i'm <span className="bold">26</span>, and I'm currently pursuing a <span className="bold">Software Engineering Technology Advanced Diploma</span> at Centennial College. I have hands-on experience in full-stack development, including proficiency in <span className="bold">C#</span>, <span className="bold">JavaScript</span>, <span className="bold">SQL</span>, and web technologies like <span className="bold">HTML</span> and <span className="bold">CSS</span>. I've <span className="bold">developed</span> and <span className="bold">maintained</span>websites, <span className="bold">designed databases</span>, and collaborated on team projects using <span className="bold">Agile</span> and <span className="bold">Scrum</span> methodologies. I'm particularly interested in leveraging my technical and problem-solving skills to contribute to a dynamic development team, and I'm seeking a co-op opportunity to further enhance my practical experience.
             </p>
         {/* button to view resume */}
-        <a href="MartaPolishchuk_Resume.pdf" target="blank" rel="noopener noreferrer">
+        <a href="Resume.pdf" target="blank" rel="noopener noreferrer">
         <button>View Resume</button></a>
       </div>
     </section>
