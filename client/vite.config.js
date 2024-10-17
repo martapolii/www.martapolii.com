ac733b5b9a784bbb15ee929e0963593c3341d16e
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-16 21:44:24 -0400

    Fix entry point and output folder paths

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,20 @@
+//vite.config.js
+
 //Configuring Vite for React
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from 'path'; // to use path.resolve
+
 
 export default defineConfig({
-  root: 'client', // setting the root directory to the current directory
+  root: '.', // setting the root directory to the current directory
   plugins: [react()],
   build: {
-    outDir: '../dist', // setting the output directory in the dist folder in the root
+    outDir: path.resolve(__dirname, '../dist'), // absolute path to the root dist folder
     emptyOutDir: true, // clears the output directory before building
     manifest: true,
     rollupOptions: {
-      input: "./src/main.jsx", // setting the entry point
+      input: "./index.html" // setting the entry point to index.html. this way it is copied into the dist folder. 
     },
   },
 });
