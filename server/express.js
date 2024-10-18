b3ceee2cabede30f908223ddbfbc6c250fb59235
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-18 13:16:14 -0400

    update express.js to serve template from root

--- server/express.js
+++ server/express.js
@@ -6,8 +6,17 @@ import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
+import Template from './../template.js' //import the template.js file do its served at the root URL
+
 
 const app = express()
+
+//serve template at the root URL
+app.get('/', (req, res) => {
+  res.status(200).send(Template()) 
+  })
+
+  
 //configure express  
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
