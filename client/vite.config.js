1ac8355e27d3200e5f03b7592c57910dac0a1d50
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-18 11:41:27 -0400

    FIX: update root and input paths in viteconfig.js, solved issue of front end not running in back end after moving server.js file

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,14 +7,14 @@ import path from 'path'; // to use path.resolve
 
 
 export default defineConfig({
-  root: '.', // setting the root directory to the current directory
+  root: './client', // setting the root directory to the current directory
   plugins: [react()],
   build: {
     outDir: path.resolve(__dirname, '../dist'), // absolute path to the root dist folder
     emptyOutDir: true, // clears the output directory before building
     manifest: true,
     rollupOptions: {
-      input: "./index.html" // setting the entry point to index.html. this way it is copied into the dist folder. 
+      input: "./client/index.html" // setting the entry point to index.html. this way it is copied into the dist folder. 
     },
   },
 });
