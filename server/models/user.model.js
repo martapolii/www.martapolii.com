d549158d7b4f1a9d6f5ff1e0ba8cc59c1f3e8461
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-20 12:01:22 -0400

    Create user model with Mongoose

---
+++ server/models/user.model.js
@@ -0,0 +1,42 @@
+//create a model for the users
+import mongoose from 'mongoose'
+
+
+const UserSchema = new mongoose.Schema({
+//namme
+  name: {
+    type: String,
+    trim: true,
+    required: 'Name is required'
+    },
+
+//email
+  email: {
+    type: String,
+    trim: true,
+    unique: 'Email already exists',
+    match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+    required: 'Email is required'
+    },
+
+//created
+  created: {
+    type: Date,
+    default: Date.now
+    },
+
+//updated
+  updated: {
+    type: Date,
+    default: Date.now
+    },
+    
+//password
+  password: {
+    type: String,
+    required: 'Password is required'
+    },
+});
+
+const User = mongoose.model('User', UserSchema);
+export default User;
