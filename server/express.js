a731701b17a774b7289fae8dbc71cb6c31c71575
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-20 12:58:10 -0400

    Import and configure routes and bodyparser middlware in express.js

--- server/express.js
+++ server/express.js
@@ -7,7 +7,10 @@ import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
 import Template from './../template.js' //import the template.js file do its served at the root URL
-
+//import the users rotes
+import userRoutes from './routes/user.routes.js'
+// import contact routes
+import contactRoutes from './routes/contact.routes.js'
 
 const app = express()
 
@@ -16,7 +19,14 @@ app.get('/', (req, res) => {
   res.status(200).send(Template()) 
   })
 
-  
+//configure express to use body-parser as middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//configure routes
+app.use('/api/users', userRoutes)
+app.use('/api/contacts', contactRoutes)
+
 //configure express  
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
