c7c39fd30f588dbc24bd3ba5ae170f2ac3c4f5ef
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-20 14:37:50 -0400

    SOLUTION to mongodb "undefined uri" error -   move dotenv.config() directly into config.js file

--- config/config.js
+++ config/config.js
@@ -1,9 +1,13 @@
 //config.js
+import dotenv from 'dotenv';
+dotenv.config();
+
 const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 5001, //listening port
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", //implementation of JWT (later stage)
   mongoUri: process.env.MONGODB_URI //location of mongodb database instance
-}
+};
+console.log('MONGODB_URI in config.js:', process.env.MONGODB_URI); //troubleshooting mongo uri being undefined in server.js
 
-export default config
+export default config;
