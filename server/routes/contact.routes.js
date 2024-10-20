98083cf9594e7643ee448bb61488c7869b33c591
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-20 12:34:44 -0400

    Create route.js file for contacts API to define the routes for handling CRUD operations

---
+++ server/routes/contact.routes.js
@@ -0,0 +1,27 @@
+//define the routes for handling all CRUD opertions for contacts API
+
+//import express
+import express from 'express'
+//import the contact controller
+import contactCtrl from '../controllers/contact.controller.js'
+//import router
+const router = express.Router()
+
+// GET - get all contacts
+//router.route('url')l.get(controllername.methodname)
+router.route('api/contacts').get(contactCtrl.read)
+
+// GET - get contacts by ID
+router.route('api/contacts/:id').get(conactCtrl.readById)
+
+// POST - add a new contact
+router.route('api/contacts').post(contactCtrl.create)
+
+// PUT - update contact by ID
+router.route('api/contacts/:id').put(contactCtrl.update)
+
+// DELETE - remove contact by ID
+router.route('api/contacts/:id').delete(contactCtrl.remove)
+
+// DELETE - remove all contacts
+router.route('api/contacts').delete(contactCtrl.removeAll)
