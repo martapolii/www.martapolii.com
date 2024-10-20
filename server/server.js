aea9bb170e955eeef2c3b10df003abe3115274b3
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-20 16:53:16 -0400

    Moved express related code to express.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ Date: October 13, 2024
 
 // Creating the server with Express
 // This file is the entry point of the server
+// main purpose: initialize the server, connect to the database, and start express app
     //should be able to use "touch server.js" to create the file
     // since this didn't work, I created a server.js file in the root directory manually 
     // this file allows us to use out frontend in the backend. this will 'break' some routes (ex static assets will no longer show, need to fix this)
@@ -20,20 +21,18 @@ Date: October 13, 2024
   import { dirname } from 'path';
   // import dotenv to use environment variables
   import dotenv from 'dotenv';
-
+  import mongoose from 'mongoose' //import mongoose to connect to MongoDB
+  import config from '../config/config.js'; //import the configuration file
+  import app from './express.js'; // import the express application
+ // import assetsRouter from './assets-router.js'; - moved to express.js
 
   // Define the __filename and __dirname variables for ES modules
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
 
-  // load environment variables from a .env file into process.env
-  dotenv.config({ path: path.resolve(__dirname, '../.env') }); // make sure dotenv is reading .env file from the root directory
-  // dotenv.config(); <- normally this would work, but since we are using a different directory structure, we need to specify the path
-
-  import mongoose from 'mongoose' //import mongoose to connect to MongoDB
-  import config from '../config/config.js'; //import the configuration file
-  import express from 'express'; // to create an express application
-  import assetsRouter from './assets-router.js'; // Import the assets router defined in assets-router.js
+   // load environment variables from a .env file into process.env
+   dotenv.config({ path: path.resolve(__dirname, '../.env') }); // make sure dotenv is reading .env file from the root directory
+   // dotenv.config(); <- normally this would work, but since we are using a different directory structure, we need to specify the path
 
 /**** MONGOOSE ****/ 
   mongoose.Promise = global.Promise
@@ -49,14 +48,9 @@ Date: October 13, 2024
     })
     
 
-// Creating the Express application:
-  const app = express(); // initializes the express application
+// Creating the Express application: 
+  //const app = express(); // initializes the express application - moved to express.js
 
-// Serving Static Files:  (**normlly don't need both below methods, but this is what worked for me)
-  //serve static files from dist
-  app.use(express.static(path.join(__dirname, "../dist"))); //middleware that serves static files from the dist folder when the root URL (/) is specified
-// Configure assets router to serve images and videos:
-  app.use("/", assetsRouter);
 
 // Display a message when the server is accessed: 
   app.get("/", (_req, res) => {
@@ -91,5 +85,5 @@ app.get("/*", (_req, res) => { //a catch-all route for requests that don't match
   });
 
 
-  console.log('Mongo URI:', config.mongoUri); // Add this to check if the value is being loaded
+
 
