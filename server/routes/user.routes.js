214ee9a6a8d995d95c95ce5d62aa94b569f7c408
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-20 16:55:15 -0400

    Fixed API routes syntax  for users

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,27 +1,30 @@
-//define the routes for handling all CRUD opertions for users API
+//define the routes and API endpoints for handling all CRUD opertions for users 
 
 //import express
-import express from 'express'
-//import the user controller
-import userCtrl from '../controllers/user.controller.js'
+import express from 'express';
+//import the user Controller  
+import userCtrl from '../controllers/user.controller.js';
+
 //import router
-const router = express.Router()
+const router = express.Router();
 
 // GET - get all users
-router.route('api/users').get(userCtrl.read)
+router.route('api/users').get(userCtrl.read);
 
 // GET - get user by ID
-router.route('api/users/:id').get(userCtrl.readById)
+router.route('api/users/:id').get(userCtrl.readById);
 
 // POST - add a new user
-router.route('api/users').get(userCtrl.create)
+router.route('api/users').post(userCtrl.create);
 
 // PUT - update user by ID
-router.route('api/users/:id').get(userCtrl.update)
+router.route('api/users/:id').put(userCtrl.update);
 
 // DELETE - remove user by ID
-router.route('api/users/:id').get(userCtrl.remove)
+router.route('api/users/:id').delete(userCtrl.remove);
 
 // DELETE - remove all users
-router.route('api/users'),get(userCtrl.removeAll)
+router.route('api/users').delete(userCtrl.removeAll);
+
+export default router;
 
