52da9dc4d9f55d27838efe1b8119292b370856f5
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-19 23:58:19 -0400

    Update environemental variables

--- server/server.js
+++ server/server.js
@@ -11,22 +11,24 @@ Date: October 13, 2024
     // since this didn't work, I created a server.js file in the root directory manually 
     // this file allows us to use out frontend in the backend. this will 'break' some routes (ex static assets will no longer show, need to fix this)
 
+
+//1. Importing the required modules:
+// had to add to use ES modules since __dirame is not available by default:
 // import dotenv to use environment variables
 import dotenv from 'dotenv';
-
-    // had to add these to use ES modules since __dirame is not available by default:
-    import { fileURLToPath } from 'url';
-    import { dirname } from 'path';
-    
+// const path = require("path"); // to work with file and directory paths
+import path from 'path';
+  import { fileURLToPath } from 'url';
+  import { dirname } from 'path';
+        
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = dirname(__filename);
-    
-//1. Importing the required modules:
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') }); // make sure dotenv is reading .env file from the root directory
+
 // const express = require("express"); // to create an express application
-// const path = require("path"); // to work with file and directory paths
-// change to import since using ES modules:
 import express from 'express';
-import path from 'path';
+
 //3. Importing the assets router defined in assets-router.js:
 //const assetsRouter = require("./assets-router");app.use("/src", assetsRouter);
 //const assetsRouter = require("./assets-router");
@@ -38,13 +40,14 @@ import config from '../config/config.js';
 import mongoose from 'mongoose' //import mongoose to connect to MongoDB
 mongoose.Promise = global.Promise
 mongoose.connect(config.mongoUri, { useNewUrlParser: true,
-useCreateIndex: true, 
-useUnifiedTopology: true } )
+//useCreateIndex: true,  // ******************************comment out after adding config file**************************
+//useUnifiedTopology: true 
+} )
 mongoose.connection.on('error', () => {
 throw new Error(`unable to connect to database: ${config.mongoUri}`) 
 })
 
-dotenv.config({ path: path.resolve(__dirname, '../.env') }); // make sure dotenv is reading .env file from the root directory
+
 //2. Creating the Express application:
 const app = express(); // initializes the express application
 
