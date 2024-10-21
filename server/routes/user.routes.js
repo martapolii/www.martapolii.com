b779dae998869c04651f1ebf06707e57f0f866ea
martapolii <mpolish3@my.centennialcollege.ca>
2024-10-20 21:55:25 -0400

    Node_modules folder re-uploaded

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,33 +1,18 @@
 //define the routes and API endpoints for handling all CRUD opertions for users 
 
-//import express
-import express from 'express';
-//import the user Controller  
-import userCtrl from '../controllers/user.controller.js';
+import express from 'express'; //import express
+import userCtrl from '../controllers/user.controller.js'; //import the user Controller
 
-//import router
-const router = express.Router();
+const router = express.Router(); //import router
 
-// GET - get all users
-router.route('/api/users').get(userCtrl.read);
+router.route('/api/users').get(userCtrl.read); // GET - get all users
+router.route('/api/users/:id').get(userCtrl.readById); // GET - get user by ID
+router.route('/api/users').post(userCtrl.create); // POST - add a new user
+router.route('/api/users/:id').put(userCtrl.update); // PUT - update user by ID
+router.route('/api/users/:id').delete(userCtrl.remove); // DELETE - remove user by ID
+router.route('/api/users').delete(userCtrl.removeAll); // DELETE - remove all users
 
-// GET - get user by ID
-router.route('/api/users/:id').get(userCtrl.readById);
-
-// POST - add a new user
-router.route('/api/users').post(userCtrl.create);
-
-// PUT - update user by ID
-router.route('/api/users/:id').put(userCtrl.update);
-
-// DELETE - remove user by ID
-router.route('/api/users/:id').delete(userCtrl.remove);
-
-// DELETE - remove all users
-router.route('/api/users').delete(userCtrl.removeAll);
-
-// middleware to fetch the user by id:
-router.param('id', userCtrl.userByID);
+router.param('id', userCtrl.userByID); // middleware to fetch the user by id
 
 export default router;
 
